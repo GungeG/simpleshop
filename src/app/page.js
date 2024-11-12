5fb7cde01e26126782d24603314f00aa5a3730f1
axwii <axwi0001@stud.kea.dk>
2024-11-12 18:31:49 +0100

    Enhance home page with background image and improved layout for better user experience

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 
-
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Link href="/products">products</Link>
-
+    <div className="relative h-screen w-full">
+      <Image 
+        src={"/images/kiosk.jpg"} 
+        layout="fill" 
+        objectFit="cover" 
+        alt="Kiosk" 
+        className="z-0"
+      />
+      <div className="relative z-10 flex flex-col items-center justify-center h-full bg-black bg-opacity-50 text-white">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">Kiosk Kompagniet</h1>
+        <Link className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300" href="/products">products</Link>
+      </div>
     </div>
   );
 }
