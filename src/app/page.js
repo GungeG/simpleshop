09453bd3fc3ce21e8352b3cc46909afcf5f6c161
axwii <axwi0001@stud.kea.dk>
2024-11-12 14:16:06 +0100

    Update Next.js dependency to allow minor version upgrades and add product listing and detail pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div> </div>
+    <div> 
+      <h1>Home</h1>
+      <Link href="/products">products</Link>
+    </div>
   );
 }
