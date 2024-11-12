f8bd00746b8d9665564bcdb7bfd9e6f1d39e22cb
axwii <axwi0001@stud.kea.dk>
2024-11-12 14:47:04 +0100

    Add image handling for product pages and configure remote image patterns

--- src/app/details/[id]/page.jsx
+++ src/app/details/[id]/page.jsx
@@ -1,8 +1,18 @@
+import Image from 'next/image';
+
 const Page = async ({ params }) => {
     const id = (await params).id;
     let response = await fetch(`https://dummyjson.com/products/${id}`);
     let product = await response.json();
-    return <div>{product.title}</div>;
+    return <div>
+      {product.title}
+      <Image
+        src={product.thumbnail}
+        width={250}
+        height={250}
+        alt={product.title}
+      />
+      </div>;
   };
   
   export default Page;
