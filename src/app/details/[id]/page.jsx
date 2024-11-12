7a846d4089350ca9ff743249d7321beca05117b2
axwii <axwi0001@stud.kea.dk>
2024-11-12 16:15:59 +0100

    Enhance product details page with price and reviews display

--- src/app/details/[id]/page.jsx
+++ src/app/details/[id]/page.jsx
@@ -1,19 +1,37 @@
-import Image from 'next/image';
+'use server'; 
+
+import Image from "next/image";
 
 const Page = async ({ params }) => {
-    const id = (await params).id;
-    let response = await fetch(`https://dummyjson.com/products/${id}`);
-    let product = await response.json();
-    return <div>
-      {product.title}
+  const id = (await params).id;
+  let response = await fetch(`https://dummyjson.com/products/${id}`);
+  let product = await response.json();
+  return (
+    <div>
+<h1>{product.title}</h1>
       <Image
         src={product.thumbnail}
         width={250}
         height={250}
         alt={product.title}
       />
-      {product.description}
-      </div>;
-  };
-  
-  export default Page;
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
+      <section>Rating: {product.rating}</section>
+      <div>
+        <h2>Reviews</h2>
+        {product.reviews.map((review, index) => (
+          <div key={index}>
+            <p>Rating: {review.rating}</p>
+            <p>Date: {new Date(review.date).toLocaleDateString()}</p>
+            <p>Name: {review.reviewerName}</p>
+            <p>Email: {review.reviewerEmail}</p>
+            <p>Comment: {review.comment}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
