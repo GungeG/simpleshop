ac70d146c3e90d79f9231eaa5e66a6c2b28f06f1
Nikolaj <nikolajgottschalck@hotmail.com>
2024-11-12 14:57:35 +0100

    footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,9 +2,7 @@
 
 const Footer = () => {
   return (
-    <p className="leading-8 text-[25px] mb-4">
-      We use cookies to improve your user expirence.
-    </p>
+<div className="text-base border-solid border-2 m-2 border-blue-900 rounded-lg p-1 text-center"><p>2024©</p></div>
   );
 };
 
