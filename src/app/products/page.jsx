890646294d09bfec94e5f71a50880f3c401fcd53
Nikolaj <nikolajgottschalck@hotmail.com>
2024-11-13 21:54:22 +0100

    link igen

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -8,18 +8,17 @@ export default async function Home() {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-16 items-center justify-items-center min-h-screen p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
       {data.products.map((product) => {
         return (
-          <div
-            key={product.id}
-            className="p-3 rounded-lg shadow-sm bg-blue-800"
-          >
-            <Image
-              src={product.thumbnail}
-              width={250}
-              height={250}
-              alt={product.title}
-            />
-            <Link href={`/details/${product.id}`}>{product.title}</Link>
-          </div>
+          <Link href={`/details/${product.id}`} key={product.id}>
+            <div className="p-3 rounded-lg shadow-sm bg-blue-800">
+              <Image
+                src={product.thumbnail}
+                width={250}
+                height={250}
+                alt={product.title}
+              />
+              {product.title}
+            </div>
+          </Link>
         );
       })}
     </div>
