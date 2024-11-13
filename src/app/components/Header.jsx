2ce1e93e4fc35aeb4ac651bb34e42d3f75d71bef
Nikolaj <nikolajgottschalck@hotmail.com>
2024-11-13 12:21:06 +0100

    svg basket header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,21 +1,27 @@
 "use client";
 import Link from 'next/link';
+import { SlBasket } from "react-icons/sl";
 
 const Header = () => {
   return (
-    <div className="text-base border-solid border-2 m-2 border-blue-900 rounded-lg p-1">
-      <ul className="flex space-x-8 ml-8">
-        <li>
-          <Link href="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/products">
-            Products
-          </Link>
-        </li>
-      </ul>
+    <div className="text-base border-solid border-2 m-2 border-blue-900 rounded-lg p-2">
+      <div className="flex justify-between items-center">
+        <ul className="flex space-x-8 ml-8">
+          <li>
+            <Link href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/products">
+              Products
+            </Link>
+          </li>
+        </ul>
+        <Link href="/payment">
+        <SlBasket className="mr-8 text-2xl"/>
+        </Link>
+      </div>
     </div>
   );
 };
