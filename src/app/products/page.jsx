0421a112c64495a96b88d098078f917db20283d3
axwii <axwi0001@stud.kea.dk>
2024-11-14 00:56:38 +0100

    Refactor product card layout for improved accessibility and styling

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -8,22 +8,24 @@ export default async function Home() {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-16 items-center justify-items-center min-h-screen p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
       {data.products.map((product) => {
         return (
-          <Link href={`/details/${product.id}`} key={product.id}>
-            <div className="p-3 rounded-lg shadow-sm bg-blue-800 flex flex-col justify-between">
-              <Image
-                src={product.thumbnail}
-                width={250}
-                height={250}
-                alt={product.title}
-              />
-              <div className="w-56 h-12 flex items-end">
-                {product.title}
+          <div key={product.id} className="p-3 rounded-lg shadow-sm bg-blue-800 flex flex-col justify-between">
+            <Link href={`/details/${product.id}`} >
+              <div className="bg-black p-2 rounded-xl">
+                <Image
+                  src={product.thumbnail}
+                  width={250}
+                  height={250}
+                  alt={product.title}
+                />
+                <div className="w-56 h-12 flex items-end text-white">
+                  {product.title}
+                </div>
               </div>
-              <div className="text-center mt-2">
-                <button className="p-2 bg-black rounded-xl w-56">Add to cart</button>
-              </div>
+            </Link>
+            <div className="text-center mt-2">
+              <button className="p-2 bg-black rounded-xl w-56">Add to cart</button>
             </div>
-          </Link>
+          </div>
         );
       })}
     </div>
