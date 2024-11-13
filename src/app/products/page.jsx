c779b0a2de72c1d392f57965ee0de9e66fe486ef
Nikolaj <nikolajgottschalck@hotmail.com>
2024-11-13 11:48:15 +0100

    fix af fgrids

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function Home() {
   let response = await fetch("https://dummyjson.com/products");
   let data = await response.json();
   return (
-    <div className="grid grid-cols-[1fr_1fr_1fr] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-16 items-center justify-items-center min-h-screen p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
       {data.products.map((product) => {
         return (
           <div
