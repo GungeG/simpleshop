9e3b9a810cd76335bac741207a3d1f481453edf3
axwii <axwi0001@stud.kea.dk>
2024-11-15 11:11:43 +0100

    Fix product link text case and implement query parameter display in payment page

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
       />
       <div className="relative z-10 flex flex-col items-center justify-center h-full bg-black bg-opacity-50 text-white">
         <h1 className="text-4xl md:text-6xl font-bold mb-4">Kiosk Kompagniet</h1>
-        <Link className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300" href="/products">products</Link>
+        <Link className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300" href="/products">Products</Link>
       </div>
     </div>
   );
